007fa15ff724595721f5e5226654065a1385ddd9
Francisco Suero <suerofrancisco@yahoo.com>
2020-03-21 11:48:42 -0400

    Removes our dependency on routing requests through the DOI servers

--- scraper.py
+++ scraper.py
@@ -1,17 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 import csv 
-from crossref.restful import Works
 import time
+import json
 
 
 
 def main(path, target="../covid", mode="files"):
   print("Initalizing scraping in " + mode +  "\n")
-  collection = Works()
-  scrape(path, collection)
+  scrape(path)
 
-def scrape(path, collection):
+def scrape(path):
   print("loading csv!")
   
   with open(path, 'r') as file:
@@ -20,20 +19,22 @@ def scrape(path, collection):
     count = 0
     for row in reader:
       count += 1
-      doi = collection.doi(row[10])
-      if(doi != None):
-        url = doi.get("URL", "")
+      res = requests.get("https://doi.org/api/handles/" + row[10])
+      url = json.loads(res.content).get("values", [{}])[0].get("data", {}).get("value", "")
+      print(json.loads(res.content).get("values", [{}])[0].get("data", {}))
+      if(url != ""):
         download_url(url)
       if(count % 100 == 0):
         print(count)
     print("Scraped!")
 
 def download_url(url):
-  session = requests.Session()
-  time.sleep(10)
-  res = session.get(url)
-  content = res.content
-  print(content)
-
+  print(url)
+  try:
+    res = requests.get(url)
+    content = res.content
+    print(content)
+  except:
+    print("error")
 
 main("csv.csv")
