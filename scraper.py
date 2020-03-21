fc01d9dda3774c7d04903799c8ac249980c972d0
Francisco Suero <suerofrancisco@yahoo.com>
2020-03-21 12:33:25 -0400

    A

--- scraper.py
+++ scraper.py
@@ -18,22 +18,57 @@ def scrape(path, collection):
     print("CSV Open! Initalizing scrape!")
     reader = csv.reader(file)
     count = 0
+    domain_count = {}
+    error_tracker = {}
     for row in reader:
       count += 1
-      doi = collection.doi(row[10])
-      if(doi != None):
-        url = doi.get("URL", "")
-        download_url(url)
-      if(count % 100 == 0):
+      doi = row[10]
+      if(doi != ""):
+        r = download_url("http://dx.doi.org/" + row[10])
+        if(r["error"]):
+          v = error_tracker.get(r["domain"], "")
+          if(v != ""):
+            error_tracker[r["domain"]] += 1
+          else: 
+            error_tracker[r["domain"]] = 1
+        v = domain_count.get(r["domain"], "")
+        if(v != ""):
+          domain_count[r["domain"]] += 1
+        else: 
+          domain_count[r["domain"]] = 1
+
+
+      if(count % 10 == 0):
         print(count)
     print("Scraped!")
+    print(error_tracker)
+    print(domain_count)
 
 def download_url(url):
-  session = requests.Session()
-  time.sleep(10)
-  res = session.get(url)
-  content = res.content
-  print(content)
+  domain = "error"
+  code = 0
+  try:
+    session = requests.Session()
+    res = session.get(url)
+    content = res.content
+    code = res.status_code
+    domain = res.url.split("/")[2]
+  except:
+
+    print("Error error error")
+
+  return_val = {}
+  if(code == 403):
+      return_val["domain"] = domain, 
+      return_val["error"] = True
+  elif(code == 0):
+      return_val["domain"] = "doi", 
+      return_val["error"] = True
+  else:
+      return_val["domain"] = domain, 
+      return_val["error"] = False
+  return  return_val
+
 
 
 main("csv.csv")
